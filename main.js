3b1807eae8a7ef786254fade3ea820fb6c990a7e
Ali Sattar <alisattarbarani@outlook.com>
2021-06-06 22:05:28 +0300

    First Upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var mobileMenu = document.getElementById("mobile-menu");
+
+function openMobileMenu() {
+  mobileMenu.style.display = "block";
+}
+
+function closeMobileMenu() {
+  mobileMenu.style.display = "none";
+}
